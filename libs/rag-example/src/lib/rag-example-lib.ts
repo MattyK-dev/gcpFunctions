ec561b16203638271049f09ce1493e13c3cb619a
MattyK-dev <matthew.kershaw@epiuse.com>
2024-09-15 17:27:14 +0200

    add context to question

--- libs/rag-example/src/lib/rag-example-lib.ts
+++ libs/rag-example/src/lib/rag-example-lib.ts
@@ -58,7 +58,7 @@ export async function ragExampleLib(body: any): Promise<string> {
       },
       {
         role: 'user',
-        content: 'Using the context: ' + +'Answer the question: ' + question,
+        content: 'Using the context: ' + contextString +'Answer the question: ' + question,
       },
     ],
   });
